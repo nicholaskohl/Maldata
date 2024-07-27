968fe22fda69b9e268d9a43d77ea3f9728095f7a
Nicholas <nicholaskohlbecker@gmail.com>
2024-07-27 00:11:54 -0400

    The app works now!!!! will add comments and readme next

--- main.py
+++ main.py
@@ -1,39 +1,102 @@
 from tkinter  import *
 from tkinter import ttk
+import tkinter as tk
 import pandas as pd
 import xml.etree.ElementTree as ET 
 import Maldata as md
 
 
 
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("My anime list Data Analyzer")
+        
+        self.filename = tk.StringVar()
+
+        self.start_page = StartPage(self, self.filename)
+        self.page1 = Page1(self,self.filename)
+
+        self.start_page.grid(row=0, column=0, sticky=("N W E S"))
+        self.page1.grid(row=0,column=0, sticky=("N W E S"))
+        
+        self.show_frame(self.start_page)
+
+    def show_frame(self,frame):
+        frame.tkraise()
+
 #This is the Setup for the start page, the user enters the file name of their animelist, which lets the program do it's work
 class StartPage(ttk.Frame):
-     def __init__(self,root, controller):
-        ttk.Frame.__init__(self,root)
-        root.title("Enter in the file name")
+     def __init__(self,root, filename):
+        super().__init__(root) 
         
-        mainframe = ttk.Frame(root)
+        self.root = root
+        self.filename = filename
+
+        root.title("Enter in the file name")
+
+        mainframe = ttk.Frame(self)
         mainframe.grid(column=0, row=0 , sticky=("N W E S"))
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
         
-        self.filename = StringVar()
+
         filename_entry = ttk.Entry(mainframe, width=7, textvariable=self.filename)
         filename_entry.grid(column=2,row=1,sticky=("W E"))
         ttk.Label(mainframe,text="Enter your filename").grid(column=1, row=1)
-        ttk.Button(mainframe,text="continue", command=lambda: controller.show_frame(Page1)).grid(column=2,row=2)
+        ttk.Button(mainframe,text="continue", command=self.load_data).grid(column=2,row=2)
          
         for child in mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
         filename_entry.focus()
 
+     def load_data(self):
+        try:
+            filename = self.filename.get()
+            #This is setting up the parser for my data specfically 
+            #ToDo figure out how to make this dynamic to the user
+    
+            xmlp: ET.XMLParser = ET.XMLParser(encoding="UTF-8")
+            tree: ET.ElementTree = ET.parse(source=filename, parser=xmlp)
+            Aroot: ET.Element = tree.getroot()
+
+            data: list[dict[str, any]] = []
+
+            for anime in Aroot.findall('anime'):
+                series_id: str = anime.find('series_animedb_id').text
+                title: str = anime.find('series_title').text
+                type_: str = anime.find('series_type').text
+                episodes: int = int(anime.find('series_episodes').text)
+                watched: int = int(anime.find('my_watched_episodes').text)
+                score: int = int(anime.find('my_score').text)
+                status: str = anime.find('my_status').text
+    
+                data.append({
+                    "series_id": series_id,
+                    "title": title,
+                    "type": type_,
+                    "episodes": episodes,
+                    "watched": watched,
+                    "score": score,
+                    "status": status,
+                    })
+            global dataf 
+            dataf = pd.DataFrame(data)
+            self.root.show_frame(self.root.page1)
+        except Exception as e:
+            print(f"Error loading file: {e}")
+
+
 #This is the setup for Page 1
 class Page1(ttk.Frame):
-    def __init__(self,root):
+    def __init__(self,root, filename):
+        super().__init__(root)
+        self.root = root
+        self.filename = filename
         root.title("Anime Data Analyzer")
         
         #This is setting up the frame for what we're adding to it
-        mainframe = ttk.Frame(root, padding=("4 4 12 12"))
+        mainframe = ttk.Frame(self)
         mainframe.grid(column=0, row=0 , sticky=("N W E S"))
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
@@ -64,39 +127,8 @@ class Page1(ttk.Frame):
         string_value = md.toprated(dataf)
         self.text_widget.delete(1.0,END)
         self.text_widget.insert(END, string_value)
-#This is setting up the parser for my data specfically 
-#ToDo figure out how to make this dynamic to the user
 
-xmlp: ET.XMLParser = ET.XMLParser(encoding="UTF-8")
-tree: ET.ElementTree = ET.parse(source="animelist_nicholas.xml", parser=xmlp)
-Aroot: ET.Element = tree.getroot()
-
-data: list[dict[str, any]] = []
-
-for anime in Aroot.findall('anime'):
-    series_id: str = anime.find('series_animedb_id').text
-    title: str = anime.find('series_title').text
-    type_: str = anime.find('series_type').text
-    episodes: int = int(anime.find('series_episodes').text)
-    watched: int = int(anime.find('my_watched_episodes').text)
-    score: int = int(anime.find('my_score').text)
-    status: str = anime.find('my_status').text
-
-    data.append({
-        "series_id": series_id,
-        "title": title,
-        "type": type_,
-        "episodes": episodes,
-        "watched": watched,
-        "score": score,
-        "status": status,
-        })
-
-    dataf: pd.DataFrame = pd.DataFrame(data)
 #after the Dataframe is made using all the Data in the XML file we make the gui with these commands and previous classes
-#
-root = Tk()
-Page1(root)
-root.mainloop()
-
-
+if __name__ == "__main__":
+    root = App()
+    root.mainloop()
